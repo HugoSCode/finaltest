d89c0c7e779b83a908700f8690656c13c9641645
Hugo Smith (1000125766) <SMITHT8@student.op.ac.nz>
2024-05-16 15:28:01 +1200

    my change

--- Project/Program.cs
+++ Project/Program.cs
@@ -4,10 +4,28 @@
     {
         static void Main(string[] args)
         {
-           
+            one();
+            two();
+            three();
         }
         static void one() 
-        { 
+        {
+            double bodytemp = 36.5;
+            double tempincr;
+            int count = 0;
+            Random rand = new Random();
+            Console.WriteLine("what is your body temperature?");
+            while (bodytemp < 38)
+            {
+                tempincr = rand.NextDouble();
+                bodytemp = bodytemp + tempincr;
+                Thread.Sleep(1000);
+                count++;
+                Console.WriteLine($"You have been in the sauna for {count} mins, your temperature is {bodytemp:F2}");
+            }
+            Console.WriteLine("You are too hot for the sauna");
+            Console.ReadLine();
+
         }
         static void two()
         {
